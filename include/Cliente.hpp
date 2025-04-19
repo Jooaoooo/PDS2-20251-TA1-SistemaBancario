7986f13e71c585a584534a792583d40072b3ee8d
Lucas Miller <lucas139miller@gmail.com>
2025-04-18 22:13:20 -0300

    Classe cliente criada e inicializada, pequenas modificações nos arquivos hpp

---
+++ include/Cliente.hpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include"./ContaPf.hpp"
+#include"./ContaPj.hpp"
+class Cliente{
+private:
+    std::string nome_;
+    std::string cpf_;
+    std::string rg_;
+    std::string senha_;
+    std::string endereço_;
+    std::string email_;
+    int telefone_;
+public:
+    int autenticarUsuario(std::string senha, std::string rg);
+};
